9a27e57a0c29c1a47c574304badc0c2402869aa9
HYUNHO <jeny95kr@gmail.com>
2024-05-25 16:54:29 -0400

    fixed image source

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ export default function Homepage() {
     <main className={classes["homepage-main-container"]}>
       <section className={classes["homepage-content"]}>
         <h4>SO, YOU WANT TO TRAVEL TO</h4>
-        <h1>SPACE</h1>
+        <h1>IMAGE</h1>
         <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
