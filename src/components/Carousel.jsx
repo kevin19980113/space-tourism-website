2cae44e6cf5a7a90257de0b27295ad1889403011
HYUNHO <jeny95kr@gmail.com>
2024-05-25 12:36:25 -0400

    swipe animation added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,31 +1,57 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Carousel({ crews }) {
   const [crewIndex, setCrewIndex] = useState(0);
+  const [swipeDistance, setSwipeDistance] = useState(0);
+  const touchStartX = useRef(0);
+  const touchEndX = useRef(0);
 
-  //   function showPrevCrew() {
-  //     setCrewIndex(crewIndex - 1);
-  //     if (crewIndex === 0) {
-  //       setCrewIndex(crews.length - 1);
-  //     }
-  //   }
-  //   function showNextCrew() {
-  //     setCrewIndex(crewIndex + 1);
-  //     if (crewIndex === crews.length - 1) {
-  //       setCrewIndex(0);
-  //     }
-  //   }
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.touches[0].clientX;
+    setSwipeDistance(0);
+  };
+
+  const handleTouchMove = (e) => {
+    touchEndX.current = e.touches[0].clientX;
+    setSwipeDistance(touchEndX.current - touchStartX.current);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStartX.current - touchEndX.current > 50) {
+      setCrewIndex(crewIndex - 1);
+      if (crewIndex === 0) {
+        setCrewIndex(crews.length - 1);
+      }
+    }
+
+    if (touchStartX.current - touchEndX.current < -50) {
+      setCrewIndex(crewIndex + 1);
+      if (crewIndex === crews.length - 1) {
+        setCrewIndex(0);
+      }
+    }
+    setSwipeDistance(0);
+  };
 
   return (
     <>
       <div className="crew-info-wrapper">
-        <div className="crew-info-slider">
+        <div
+          className="crew-info-slider"
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+        >
           {crews.map((crew) => (
             <div
               key={crew.name}
               className="crew-info-item"
-              style={{ transform: `translateX(${-100 * crewIndex}%)` }}
+              style={{
+                transform: `translateX(calc(${
+                  -100 * crewIndex
+                }% - ${swipeDistance}px))`,
+              }}
             >
               <h3 className="crew-info-item-role">{crew.role.toUpperCase()}</h3>
               <h1 className="crew-info-item-name">{crew.name.toUpperCase()}</h1>
@@ -45,14 +71,23 @@ export default function Carousel({ crews }) {
           ))}
         </div>
       </div>
-      <figure className="crew-img-slider">
+      <figure
+        className="crew-img-slider"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
         {crews.map((crew) => (
           <img
             src={crew.images.webp}
             alt={crew.name}
             className="crew-img"
             key={crew.name}
-            style={{ transform: `translateX(${-100 * crewIndex}%)` }}
+            style={{
+              transform: `translateX(calc(${
+                -100 * crewIndex
+              }% - ${swipeDistance}px))`,
+            }}
           ></img>
         ))}
       </figure>
