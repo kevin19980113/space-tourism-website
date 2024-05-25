3a100022e68931fe3aad3f8cfe5c6b20afb1865a
HYUNHO <jeny95kr@gmail.com>
2024-05-25 14:46:10 -0400

    technology section

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,3 +1,76 @@
+import { data } from "../data.js";
+import { useState } from "react";
+
 export default function Technology() {
-  return <h1>Technology</h1>;
+  const [technologyIndex, setTechnologyIndex] = useState(0);
+
+  function handleClickStep(stepNumber) {
+    setTechnologyIndex(stepNumber);
+  }
+
+  return (
+    <main className="technology-main-container">
+      <h4 className="technology-title">
+        <span className="number">03</span> SPACE LAUNCH 101
+      </h4>
+      <section className="technology-content">
+        <div className="technology-info-wrapper">
+          <div className="technology-step">
+            <button
+              className={`technology-step-number ${
+                technologyIndex >= 0 ? "step-active " : ""
+              }`}
+              onClick={() => handleClickStep(0)}
+            >
+              1
+            </button>
+            <div className="step-line">
+              <div
+                className={`progress-bar ${technologyIndex >= 1 ? "fill" : ""}`}
+              ></div>
+            </div>
+            <button
+              className={`technology-step-number ${
+                technologyIndex >= 1 ? "step-active " : ""
+              }`}
+              onClick={() => handleClickStep(1)}
+            >
+              2
+            </button>
+            <div className="step-line">
+              <div
+                className={`progress-bar ${technologyIndex >= 2 ? "fill" : ""}`}
+              ></div>
+            </div>
+            <button
+              className={`technology-step-number ${
+                technologyIndex === 2 ? "step-active " : ""
+              }`}
+              onClick={() => handleClickStep(2)}
+            >
+              3
+            </button>
+          </div>
+          <div
+            className="technology-description"
+            key={data.technology[technologyIndex].name}
+          >
+            <p>THE TECHNOLOGY...</p>
+            <h1>{data.technology[technologyIndex].name.toUpperCase()}</h1>
+            <p>{data.technology[technologyIndex].description}</p>
+          </div>
+        </div>
+        <figure
+          className="technology-img-wrapper"
+          key={data.technology[technologyIndex].name}
+        >
+          <img
+            src={data.technology[technologyIndex].images.portrait}
+            alt={data.technology[technologyIndex].name}
+            className="technology-img"
+          />
+        </figure>
+      </section>
+    </main>
+  );
 }
